e54c2c8cab1b643e294e10b5219c02889edb4fef
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-01-29 16:08:38 +0100

    colours

--- wodge.js
+++ wodge.js
@@ -1,12 +1,27 @@
-exports.extend = function extend(obj, srcObj){
+exports.extend = function(obj, srcObj){
     for (var prop in srcObj){
         obj[prop] = srcObj[prop];
     }
     return obj;
 };
 
-exports.escapeRegExp = function escapeRegExp(string){
+exports.escapeRegExp = function(string){
     return string
         ? string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1")
         : "";
+}
+
+exports.red = function(txt){ 
+    return "\x1b[31m" + txt + "\x1b[0m"; 
+};
+exports.green = function(txt){ 
+    return "\x1b[32m" + txt + "\x1b[0m"; 
+};
+
+exports.pluck = function(object, fn){
+    var output = [];
+    for (var prop in object){
+        if (fn(object[prop])) output.push(prop);
+    }
+    return output;
 }
