d9c0e05f07d596951e2493b9196c3a9acaa5730c
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-02-01 13:47:02 +0100

    lint, colours, array

--- wodge.js
+++ wodge.js
@@ -1,3 +1,4 @@
+"use strict";
 exports.extend = function(obj, srcObj){
     for (var prop in srcObj){
         obj[prop] = srcObj[prop];
@@ -9,13 +10,34 @@ exports.escapeRegExp = function(string){
     return string
         ? string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1")
         : "";
-}
-
-exports.red = function(txt){ 
-    return "\x1b[31m" + txt + "\x1b[0m"; 
 };
-exports.green = function(txt){ 
-    return "\x1b[32m" + txt + "\x1b[0m"; 
+
+exports.black = function(txt){
+    return "\x1b[30m" + txt + "\x1b[0m";
+};
+exports.red = function(txt){
+    return "\x1b[31m" + txt + "\x1b[0m";
+};
+exports.green = function(txt){
+    return "\x1b[32m" + txt + "\x1b[0m";
+};
+exports.yellow = function(txt){
+    return "\x1b[33m" + txt + "\x1b[0m";
+};
+exports.blue = function(txt){
+    return "\x1b[34m" + txt + "\x1b[0m";
+};
+exports.magenta = function(txt){
+    return "\x1b[35m" + txt + "\x1b[0m";
+};
+exports.cyan = function(txt){
+    return "\x1b[36m" + txt + "\x1b[0m";
+};
+exports.white = function(txt){
+    return "\x1b[37m" + txt + "\x1b[0m";
+};
+exports.bold = function(txt){
+    return "\x1b[1m" + txt + "\x1b[0m";
 };
 
 exports.pluck = function(object, fn){
@@ -24,7 +46,7 @@ exports.pluck = function(object, fn){
         if (fn(object[prop])) output.push(prop);
     }
     return output;
-}
+};
 
 exports.isNumber = function(n){
     return !isNaN(parseFloat(n)) && isFinite(n);
@@ -39,6 +61,10 @@ exports.arrayify = function(arr){
     } else {
         return Array.isArray(arr) ? arr : [ arr ];
     }
+};
+
+exports.array = function(a, n) {
+    return Array.prototype.slice.call(a, n || 0);
 };
 
 exports.every = function(obj, callback){
